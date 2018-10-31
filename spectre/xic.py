f71a6de983e5542bda893864810ba3e653723ddb
Jiří Novotný <novotng@gmail.com>
2018-10-31 17:26:21 +0100

    Main Spectra class updated for convenience functions

--- spectre/xic.py
+++ spectre/xic.py
@@ -1,9 +1,22 @@
 class Xic:
-    def __init__(self, data, rts, mz_scales):
+    def __init__(self, data, mz_scales, rt_scales):
         self.data = data
-        self.rts = rts
         self.mz_scales = mz_scales
+        self.rt_scales = rt_scales
 
     def to_pickle(self, file: str):
-        from spectre.load import to_pickle
+        from .load import to_pickle
         to_pickle(self, file)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({repr(self.data)})"
+
+    @classmethod
+    def from_pickle(cls, file: str):
+        from .load import from_pickle
+        return from_pickle(file)
