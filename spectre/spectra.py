40b06e4d790828646697cf52ee90be8eca16b3dd
Jiří Novotný <novotng@gmail.com>
2018-10-31 17:28:44 +0100

    Sampling of centroided spectra

---
+++ spectre/spectra.py
@@ -0,0 +1,27 @@
+import numpy as np
+from numba import njit
+from scipy.sparse import csr_matrix
+
+from spectre.sparse import sort_cols
+from spectre.sparse.indexing import merge_cols
+
+
+@njit
+def _max_poll(array):
+    return np.nanmax(array)
+
+
+def sample_spectra(spectra: np.ndarray, peaks: np.ndarray, values: np.ndarray,
+                   sampling: float) -> csr_matrix:
+    rows = spectra
+
+    cols = np.multiply(peaks, 1 / sampling)
+    cols = np.round(cols, out=cols).astype(np.int64, copy=False)
+    cols = np.subtract(cols, cols.min(), out=cols)
+
+    data = values.astype(np.min_scalar_type(values))
+
+    sort_cols(rows, cols, data)
+    rows, cols, data = merge_cols(rows, cols, data, _max_poll)
+
+    return csr_matrix((data, cols, rows), dtype=data.dtype)
